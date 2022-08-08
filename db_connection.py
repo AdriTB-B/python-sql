6d6e003734aaeace26b6f42b8f740856f82da876
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-08-08 11:01:01 +0200

    Terminado

--- db_connection.py
+++ db_connection.py
@@ -1,17 +1,28 @@
 import pyodbc
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
-
-def connection():
-    driver='{ODBC Driver 17 for SQL server}'
-    server='L2205014'
-    db='empresas'
-    uid='BOSONIT\adrian.tena'
-    cnx_str = (f"""Driver={driver};
+# Configuración
+driver='{ODBC Driver 17 for SQL server}'
+server='L2205014'
+db='empresas'
+uid='BOSONIT\adrian.tena'
+pwd=''
+cnx_str = (f"""Driver={driver};
                 Server={server};
                 Database={db};
                 Trusted_Connection=yes;
-                UID={uid};""")
+                UID={uid};
+                PWD={pwd}""")
+cnx_url = URL.create("mssql+pyodbc", query={"odbc_connect": cnx_str})
+
+
+def connection():
     try:
         return pyodbc.connect(cnx_str)
     except pyodbc.Error as error:
         print(error)
+
+def get_engine():
+    return create_engine(cnx_url)
+
--- operaciones.py
+++ operaciones.py
@@ -1,20 +1,26 @@
 import db_connection
 import pandas as pd
 
+
 with db_connection.connection() as conn:
     # Operaciones
     # Obtener empresas
-    df_empresas = pd.read_sql('select * from dbo.empresas',conn)
+    df_empresas = pd.read_sql('select * from dbo.proy_empresa',conn,index_col='id')
     print(f'Empresas\n{df_empresas}')
 
     # Obtener regulacion europea
-    df_eeuu_reg = pd.read_sql('select * from dbo.eeuu_reg',conn)
-    print(f'Regulacion europea\n{df_eeuu_reg}')
+    df_ue_reg = pd.read_sql('select * from dbo.proy_reg_ue',conn, index_col='id')
+    print(f'Regulacion europea\n{df_ue_reg}')
 
     # Añadir columna a empresas
-    # conn.execute("""alter table dbo.empresas add interes numeric(18,0)""")
-    df_empresas['interes'] = df_empresas['prestamo'] * (df_empresas['porcentaje']/100)
+    # conn.execute("""alter table dbo.proy_empresa add interes as prestamo * porcentaje""")
     print(df_empresas)
-    df_empresas.to_sql(name='dbo.empresas',con=conn, if_exists='replace')
+
+with db_connection.get_engine().connect() as conn:
+    # Crear tabla común con SQLAlchemy
+    df_proy_ue = df_empresas[df_empresas['nombre_proyecto'].isin(df_ue_reg['nombre_proyecto'])]
+    print(f'Tabla común de proyectos\n{df_proy_ue}')
+    df_proy_ue.to_sql('proy_ue',conn,if_exists='replace')
+    print(conn.execute('select * from proy_ue').fetchall())
 
 
