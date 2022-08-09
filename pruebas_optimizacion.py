e475d60f88fbbe52a784b75c016034cef37f6397
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-08-09 13:31:07 +0200

    Prueba multiproceso (sin terminar)

--- pruebas_optimizacion.py
+++ pruebas_optimizacion.py
@@ -1,6 +1,39 @@
+
 import pandas as pd
 import db_connection
+import multiprocessing as mp
+import time
 
-with db_connection.get_engine().connect() as conn:
-    df_sales = pd.read_sql('select * from sales.SalesOrderDetail',conn,index_col='SalesOrderID')
-    print(df_sales)
+df_sales = None
+dfs = []
+
+def load_df(*args):    
+    sql = f'select * from sales.SalesOrderDetail order by SalesOrderDetailID OFFSET {args[0][0]} ROWS FETCH NEXT {args[0][1]} ROWS ONLY '
+    print(sql)
+    return pd.read_sql(
+        sql,
+        args[0][2],
+        index_col='SalesOrderDetailID'
+        )
+
+if __name__=='__main__':
+    start_time = time.time()
+    #########################
+    with mp.Pool(mp.cpu_count()) as pool: 
+        conn = db_connection.get_engine().connect()     
+        steps = [(x,15000,conn) for x in range(0,120000,15000)]
+        print(steps)
+        dfs = pool.map(load_df,steps)       
+   
+        df_sales = pd.concat(dfs)
+        print(df_sales.count())
+        conn.close()
+   
+        
+    #########################    
+    print(f'Demora: {time.time() - start_time}')
+    
+
+
+
+
